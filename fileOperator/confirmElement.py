46f98855d8f8e710725270215e7dc8cf4423b5f7
nyanchan <personal@nyanchangames.com>
2020-01-16 12:10:14 +0900

    All and filtered iterators for confirmations

--- fileOperator/confirmElement.py
+++ fileOperator/confirmElement.py
@@ -4,8 +4,9 @@
 #Note: All comments except these top lines will be written in Japanese. 
 """ファイルオペレーションの結果、ユーザーに対して確認が必要になった項目を管理します。"""
 class ConfirmElement(object):
-	def __init__(self,elem,msg_int,msg_str):
+	def __init__(self,elem,msg_number,msg_str):
 		self.elem=elem
+		self.msg_number=msg_number
 		self.msg_str=msg_str
 		self.response=""
 
@@ -18,3 +19,15 @@ class ConfirmationManager(object):
 
 	def __len__(self):
 		return len(self.confirmations)
+
+	def Iterate(self):
+		for elem in self.confirmations:
+			yield elem
+
+	def IterateWithFilter(self,number=None):
+		for elem in self.confirmations:
+			ok=True
+			if number is not None and elem.msg_number!=number: ok=False
+			if ok:yield elem
+		#end for
+	#end IterateWithFilter
